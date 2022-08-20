7b9d91b2a7aab173c9309fde762dfe59634ed2e8
Rob Brazier <git+github@brzr.co>
2022-08-20 15:52:52 +0100

    add firefox setup

--- .chezmoiscripts/run_once_after_asdf.fish
+++ .chezmoiscripts/run_once_after_asdf.fish
@@ -7,10 +7,10 @@ asdf list all nodejs > /dev/null
 asdf list all python > /dev/null
 
 asdf install nodejs lts-gallium
-asdf install python latest
+#asdf install python latest
 
 asdf global nodejs (asdf list nodejs | grep '*' | xargs echo | sed 's/*//g')
-asdf global python (asdf list python | grep '*' | xargs echo | sed 's/*//g')
+#asdf global python (asdf list python | grep '*' | xargs echo | sed 's/*//g')
 
-python -m pip install --user pipx
-python -m pipx ensurepath
+#python -m pip install --user pipx
+#python -m pipx ensurepath
