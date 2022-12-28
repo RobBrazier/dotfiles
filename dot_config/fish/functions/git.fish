6cfdeb933fa8bd2d7044e997e44925c98964ddac
Rob Brazier <git+github@brzr.co>
2022-12-28 18:22:50 +0000

    add git function to set email address dynamically

--- dot_config/fish/functions/git.fish
+++ dot_config/fish/functions/git.fish
@@ -12,11 +12,47 @@ function git
                 command git $argv
             end
           else
-            echo "git $argv"
             command git $argv
           end
+        case set-email
+            set_email $PWD
         case '*'
           command git $argv
+    end
+end
+
+function build_email
+    set -l repo_hostname $argv[1]
+
+    switch $repo_hostname
+        case git.sr.ht
+            set -f email_tag srht
+        case '*'
+            set -f email_tag (string split -f1 '.' $repo_hostname)
+    end
+    echo "git+$email_tag@brzr.co"
+end
+
+function set_email
+    set -l repo_path $argv[1]
+    set -l remote_url (command git -C $repo_path remote get-url origin)
+
+    if string match -rq "git@" $remote_url
+        set -l remote_url (string replace 'git@' '' $remote_url)
+        set -f remote_hostname (string split -f1 ':' $remote_url)
+    else if string match -rq "https://" $remote_url
+        set -l remote_url (string replace 'https://' '' $remote_url)
+        set -f remote_hostname (string split -f1 '/' $remote_url)
+    end
+
+    set -l new_email (build_email $remote_hostname)
+
+    if [ (command git -C $repo_path config user.email) = $new_email ]
+        echo "Email already configured for $repo_path as $new_email"
+    else
+        echo "Setting email for $repo_path to $new_email"
+        command git -C $repo_path config --unset user.email
+        command git -C $repo_path config user.email $new_email
     end
 end
 
@@ -38,7 +74,9 @@ function clone
         case "*--help"
             command git $argv
         case "*"
-            command git clone "git@$gitHost:$gitUser/$repository" "$sourceHome/$gitHost/$gitUser/$repository"
+            set -l targetPath "$sourceHome/$gitHost/$gitUser/$repository"
+            command git clone "git@$gitHost:$gitUser/$repository" $targetPath
+            set_email $targetPath
         end
     end
 end
