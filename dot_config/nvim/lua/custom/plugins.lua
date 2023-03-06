bf7fa1c2a00f3fbaa082385b9fe8c09f203c6ac4
Rob Brazier <git+github@brzr.co>
2023-03-06 19:07:25 +0000

    update nvim config

--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -1,6 +1,14 @@
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
 return {
-	'nvim-tree/nvim-tree.lua',
-	requires = {
-		'nvim-tree/nvim-web-devicons',
-	}
-}
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+	  "nvim-lua/plenary.nvim",
+	  "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+	  "MunifTanjim/nui.nvim",
+	},
+	config = function ()
+	  require('neo-tree').setup {}
+	end,
+  }
