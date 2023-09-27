4ef72375862aad0aedd802bbcf3b3b2acbd3272c
Rob Brazier <git+github@brzr.co>
2023-09-27 22:00:36 +0100

    add go to LSP and treesitter

--- dot_config/nvim/exact_lua/plugins/languages.lua
+++ dot_config/nvim/exact_lua/plugins/languages.lua
@@ -7,7 +7,24 @@ return {
 				"tsx",
 				"typescript",
 				"svelte",
+				"go",
 			})
 		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				-- pyright will be automatically installed with mason and loaded with lspconfig
+				ruff_lsp = {},
+				gopls = {},
+			},
+		},
+	},
+	{
+		"linux-cultist/venv-selector.nvim",
+		dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
+		config = true,
+		event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
 	},
 }
