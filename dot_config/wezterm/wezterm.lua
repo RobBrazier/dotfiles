7c06d2a9e824687eaa5255ec02ba9ed6999fcc26
Rob Brazier <git+github@brzr.co>
2023-12-02 14:24:39 +0000

    update wezterm config for better fullscreen and transparency

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -7,11 +7,14 @@ end
 
 config.color_scheme = "Catppuccin Mocha"
 
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.95
 
 config.automatically_reload_config = true
 
-config.hide_tab_bar_if_only_one_tab = true
+-- config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+
+config.window_decorations = "INTEGRATED_BUTTONS"
 
 config.window_padding = {
 	left = 5,
@@ -22,5 +25,38 @@ config.window_padding = {
 config.initial_rows = 38
 config.initial_cols = 149
 config.default_cursor_style = "SteadyBar"
+
+local get_transparent_color = function(hex)
+	local background = wezterm.color.parse(hex)
+	local bg_h, bg_s, bg_l, _ = background:hsla()
+	return wezterm.color.from_hsla(bg_h, bg_s, bg_l, 0.95)
+end
+
+local color_scheme = wezterm.color.get_builtin_schemes()[config.color_scheme]
+
+config.colors = {
+	tab_bar = {
+		background = get_transparent_color(color_scheme.tab_bar.inactive_tab.bg_color),
+	},
+}
+
+config.tab_max_width = 20
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	if tab.is_active then
+		return {
+			{ Background = { Color = get_transparent_color(color_scheme.tab_bar.active_tab.bg_color) } },
+			{ Text = " " .. tab.tab_index + 1 },
+			{ Text = ": " },
+			{ Text = tab.active_pane.title .. " " },
+		}
+	end
+	return {
+		{ Background = { Color = get_transparent_color(color_scheme.tab_bar.inactive_tab.bg_color) } },
+		{ Text = " " .. tab.tab_index + 1 },
+		{ Text = ": " },
+		{ Text = tab.active_pane.title .. " " },
+	}
+end)
 
 return config
