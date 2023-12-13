a964b6de1827697da4a18c1b77c4f8f1c7625db7
Rob Brazier <git+github@brzr.co>
2023-12-13 18:31:27 +0000

    fix ssh not working on non-wezterm

--- dot_config/fish/functions/ssh.fish
+++ dot_config/fish/functions/ssh.fish
@@ -1,7 +1,7 @@
 function ssh --description 'alias ssh=kitty +kitten ssh'
     if [ $TERM = xterm-kitty ]
         kitty +kitten ssh $argv
-    else if [ $TERM_PROGRAM = WezTerm ]
+    else if [ "$TERM_PROGRAM" = WezTerm ]
         wezterm ssh $argv
     else
         command ssh $argv
