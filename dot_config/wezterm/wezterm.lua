37fb7739cfb4a9877684dffe7ed628ba0bbc8a1a
Rob Brazier <git@brzr.co>
2024-02-23 17:40:31 +0000

    add wezterm config for windows

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,6 +5,10 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_prog = { "ubuntu2204.exe" }
+end
+
 config.color_scheme = "Catppuccin Mocha"
 
 config.window_background_opacity = 0.95
