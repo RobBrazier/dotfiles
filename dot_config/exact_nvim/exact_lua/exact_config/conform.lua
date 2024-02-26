2b144bf61444238807df160953e18376b92cbff1
Rob Brazier <git+github@brzr.co>
2024-02-26 20:55:16 +0000

    Update .config/nvim/lua/config/conform.lua

--- dot_config/exact_nvim/exact_lua/exact_config/conform.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/conform.lua
@@ -7,5 +7,6 @@ return {
     -- You can use a sub-list to tell conform to run *until* a formatter
     -- is found.
     -- javascript = { { "prettierd", "prettier" } },
+    templ = { 'templ' },
   },
 }
