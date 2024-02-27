d4e32275663ec3887f992c1ac069a6dcb48c1c7e
Rob Brazier <git+github@brzr.co>
2024-02-27 21:51:51 +0000

    Update .config/nvim/lua/custom/plugins/mini.lua

--- dot_config/exact_nvim/exact_lua/exact_custom/exact_plugins/mini.lua
+++ dot_config/exact_nvim/exact_lua/exact_custom/exact_plugins/mini.lua
@@ -1,51 +1,36 @@
 return {
-  -- Better Around/Inside textobjects
-  --
-  -- Examples:
-  --  - va)  - [V]isually select [A]round [)]parenthen
-  --  - yinq - [Y]ank [I]nside [N]ext [']quote
-  --  - ci'  - [C]hange [I]nside [']quote
   {
-    'echasnovski/mini.ai',
-    opts = {
-      n_lines = 500,
-    },
-  },
-  -- Add/delete/replace surroundings (brackets, quotes, etc.)
-  --
-  -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-  -- - sd'   - [S]urround [D]elete [']quotes
-  -- - sr)'  - [S]urround [R]eplace [)] [']
-  {
-    'echasnovski/mini.surround',
-    opts = {},
-  },
-  -- Simple and easy statusline.
-  --  You could remove this setup call if you don't like it,
-  --  and try some other statusline plugin
-  {
-    'echasnovski/mini.statusline',
-    opts = {},
-  },
-  -- Simple File Explorer
-  {
-    'echasnovski/mini.files',
+    'echasnovski/mini.nvim',
     dependencies = {
       'nvim-tree/nvim-web-devicons',
     },
-    keys = {
-      {
-        '<leader>fe',
-        function()
-          require('mini.files').open()
-        end,
-        desc = '[F]ile [E]xplorer',
-      },
-    },
-  },
-  -- Simple Starter Page
-  {
-    'echasnovski/mini.starter',
-    opts = {},
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]parenthen
+      --  - yinq - [Y]ank [I]nside [N]ext [']quote
+      --  - ci'  - [C]hange [I]nside [']quote
+      require('mini.ai').setup { n_lines = 500 }
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup()
+
+      -- Simple and easy statusline.
+      require('mini.statusline').setup()
+
+      -- Move lines and blocks of text up and down
+      require('mini.move').setup()
+
+      -- Simple file explorer
+      local files = require 'mini.files'
+      files.setup()
+
+      vim.keymap.set('n', '<leader>fe', files.open, { desc = '[F]ile [E]xplorer' })
+    end,
   },
 }
