8037832603d739d39741ae38227b0971f432aca6
Rob Brazier <git+github@brzr.co>
2024-02-27 22:21:13 +0000

    Update .config/nvim/lua/custom/plugins/telescope.lua

--- dot_config/exact_nvim/exact_lua/exact_custom/exact_plugins/telescope.lua
+++ dot_config/exact_nvim/exact_lua/exact_custom/exact_plugins/telescope.lua
@@ -23,7 +23,7 @@ return {
       -- Useful for getting pretty icons, but requires special font.
       --  If you already have a Nerd Font, or terminal set up with fallback fonts
       --  you can enable this
-      -- { 'nvim-tree/nvim-web-devicons' }
+      { 'nvim-tree/nvim-web-devicons' },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -51,11 +51,11 @@ return {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
