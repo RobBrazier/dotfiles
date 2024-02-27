1f158182318eab759d70bd8dc9da3965aa07f6e8
Rob Brazier <git+github@brzr.co>
2024-02-27 22:47:19 +0000

    add alpha start page

---
+++ dot_config/exact_nvim/exact_lua/exact_custom/exact_plugins/alpha.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    'goolord/alpha-nvim',
+    config = function()
+      local dashboard = require 'alpha.themes.dashboard'
+      local logo = {
+        [[                                                    ]],
+        [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+        [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+        [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+        [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+        [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+        [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+        [[                                                    ]],
+      }
+      dashboard.section.header.val = logo
+      dashboard.section.buttons.val = {
+        dashboard.button('f', ' ' .. ' Find file', '<cmd> Telescope find_files <cr>'),
+        dashboard.button('n', ' ' .. ' New file', '<cmd> ene <BAR> startinsert <cr>'),
+        dashboard.button('r', ' ' .. ' Recent files', '<cmd> Telescope oldfiles <cr>'),
+        dashboard.button('g', ' ' .. ' Find text', '<cmd> Telescope live_grep <cr>'),
+        dashboard.button('l', '󰒲 ' .. ' Lazy', '<cmd> Lazy <cr>'),
+        dashboard.button('q', '  > Quit NVIM', ':qa<CR>'),
+      }
+      require('alpha').setup(dashboard.opts)
+      vim.api.nvim_create_autocmd('User', {
+        once = true,
+        pattern = 'LazyVimStarted',
+        callback = function()
+          local stats = require('lazy').stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          dashboard.section.footer.val = '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms'
+          pcall(vim.cmd.AlphaRedraw)
+        end,
+      })
+    end,
+  },
+}
