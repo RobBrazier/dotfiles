8dea69f386df5739a5382e0d173a8bee08337f31
Rob Brazier <git+github@brzr.co>
2024-10-02 20:16:10 +0100

    fix squared style

--- .chezmoitemplates/firefox/ff-ultima-overrides.user.js
+++ .chezmoitemplates/firefox/ff-ultima-overrides.user.js
@@ -2,7 +2,7 @@ user_pref("ultima.theme.extensions", false);
 user_pref("ultima.xstyle.urlbar", false);
 user_pref("ultima.theme.icons", false);
 
-user_pref('ultima.spacing.compact', true)
+user_pref('ultima.spacing.compact', true);
 
 user_pref("ultima.xstyle.private", true);
 
