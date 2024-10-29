200538d7313756012a91b5fbed5a6752a181f175
Rob Brazier <git+github@brzr.co>
2024-10-29 18:19:45 +0000

    update direnv and source nix config if it exists

--- dot_config/exact_zsh/integrations.zsh
+++ dot_config/exact_zsh/integrations.zsh
@@ -6,20 +6,30 @@ local command_exists() {
 if command_exists 'chezmoi'; then
 	eval "$(chezmoi completion zsh)"
 fi
+
 if command_exists 'mise'; then
 	eval "$(mise activate zsh)"
 	if command_exists 'usage'; then
 		eval "$(mise completion zsh)"
 	fi
 fi
+
 if command_exists 'wezterm'; then
 	eval "$(wezterm shell-completion --shell zsh 2>/dev/null)"
 fi
-if command_exists 'direnv'; then
+
+if mise which direnv &>/dev/null; then
+	eval "$(mise x -- direnv hook zsh)"
+elif command_exists 'direnv'; then
 	eval "$(direnv hook zsh)"
 fi
 
 if mise which bob &>/dev/null; then
 	eval "$(mise x -- bob complete zsh)"
 	export PATH="$HOME/.local/share/bob/nvim-bin/:$PATH"
+fi
+
+# Nix
+if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then 
+	source "$HOME/.nix-profile/etc/profile.d/nix.sh"
 fi
