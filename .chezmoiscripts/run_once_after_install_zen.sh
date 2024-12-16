0728111730f9aa94640508e8da44313071db279f
Rob Brazier <git+github@brzr.co>
2024-12-16 16:30:15 +0000

    feat: add zen to path and align zen.desktop with appimage variant

--- .chezmoiscripts/run_once_after_install_zen.sh
+++ .chezmoiscripts/run_once_after_install_zen.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Installing Zen Browser to /opt/zen"
-wget -qO- https://github.com/zen-browser/desktop/releases/latest/download/zen.linux-specific.tar.bz2 | sudo tar xjf - -C /opt
+if ! [ -d "/opt/zen" ]; then
+	wget -qO- https://github.com/zen-browser/desktop/releases/latest/download/zen.linux-specific.tar.bz2 | sudo tar xjf - -C /opt
+else
+	echo "Zen Browser is already installed - skipping."
+fi
+
+sudo ln -sf /opt/zen/zen /usr/local/bin/zen
