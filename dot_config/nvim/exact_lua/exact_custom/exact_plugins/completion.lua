936471b8cfac95e41a8e1b19c07a497a78daa766
Rob Brazier <git+github@brzr.co>
2025-04-26 13:25:12 +0100

    enable codeium (windsurf)

--- dot_config/nvim/exact_lua/exact_custom/exact_plugins/completion.lua
+++ dot_config/nvim/exact_lua/exact_custom/exact_plugins/completion.lua
@@ -5,6 +5,15 @@ return {
     version = '1.*',
     dependencies = {
       'echasnovski/mini.nvim',
+      {
+        'Exafunction/windsurf.nvim',
+        name = 'codeium.nvim',
+        opts = {
+          enable_cmp_source = false,
+          enable_chat = false,
+          virtual_text = false,
+        },
+      },
       'folke/lazydev.nvim',
     },
     opts = {
@@ -15,19 +24,23 @@ return {
         nerd_font_variant = 'mono',
       },
       completion = {
+        ghost_text = {
+          enabled = true,
+        },
         documentation = {
           auto_show = false,
           auto_show_delay_ms = 500,
         },
       },
       sources = {
-        default = { 'lsp', 'path', 'buffer', 'snippets', 'lazydev' },
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'codeium', 'lazydev' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          codeium = { name = 'Codeium', module = 'codeium.blink', async = true },
         },
       },
       snippets = { preset = 'mini_snippets' },
-      fuzzy = { implementation = 'lua' },
+      fuzzy = { implementation = 'prefer_rust' },
       signature = { enabled = true },
     },
   },
