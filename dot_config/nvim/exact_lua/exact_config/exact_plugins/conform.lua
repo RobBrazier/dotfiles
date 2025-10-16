71434b3de853692c5fa01cc4209868d5885cd959
Rob Brazier <2453018+RobBrazier@users.noreply.github.com>
2025-10-16 13:06:32 +0100

    update ssh config, add sshct alias and fix conform config

--- dot_config/nvim/exact_lua/exact_config/exact_plugins/conform.lua
+++ dot_config/nvim/exact_lua/exact_config/exact_plugins/conform.lua
@@ -14,8 +14,5 @@ return {
     -- You can use 'stop_after_first' to run the first available formatter from the list
     -- javascript = { "prettierd", "prettier", stop_after_first = true },
     templ = { 'templ' },
-    terraform = { 'tofu_fmt' },
-    tf = { 'tofu_fmt' },
-    ['terraform-vars'] = { 'tofu_fmt' },
   },
 }
